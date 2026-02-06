763e85be3c15c0465cc93685c0752136955f02a3
Jhenifer Halma <highhalma@gmail.com>
2026-02-06 15:01:54 -0300

    switchLed updated

--- switchLed.ino
+++ switchLed.ino
@@ -1,18 +1,22 @@
 hw_timer_t *timer = NULL;
 hw_timer_t *timer2 = NULL;
+
 volatile int tempo = 0;
 int flag = 0;
+int ledPin = 22;
 
 void IRAM_ATTR onTimer() {
   tempo++;
 }
 
 void IRAM_ATTR switchLed() {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  digitalWrite(ledPin, !digitalRead(ledPin));
 }
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, 1);
+
   Serial.begin(115200);
 
   timer = timerBegin(1000000);
@@ -21,16 +25,17 @@ void setup() {
 
   timer2 = timerBegin(1000000);
   timerAttachInterrupt(timer2, &switchLed);
+  timerAlarm(timer2, 100000, true, 0);
 }
 
 void loop() {
-  if (tempo >= 5000) {
+  if (tempo >= 500) {
     tempo = 0;
     flag++;
     if (flag % 2 == 0) {
-      timerAlarm(timer2, 100000, true, 0);
+      timerAlarm(timer2, 1000000, true, 0);
     } else if (flag % 2 == 1) {
-      timerAlarm(timer2, 500000, true, 0);
+      timerAlarm(timer2, 100000, true, 0);
     }
   }
 }
