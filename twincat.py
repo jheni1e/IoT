68e5599537ba3b256da009bcd468536dbd2831a9
Jhenifer Halma <highhalma@gmail.com>
2026-02-11 13:20:58 -0300

    twincat updated

--- twincat.py
+++ twincat.py
@@ -1,7 +1,8 @@
 import pyads
 
-AMS_NET_ID = '192.168.56.1.1.1'
+AMS_NET_ID = ''
 ADS_PORT = 851
+TARGET_IP = ''
 
 plc = pyads.Connection(AMS_NET_ID, ADS_PORT, TARGET_IP)
 
@@ -9,7 +10,7 @@ with plc:
     print(f"Conexão com: {plc.ip_address}")
     
     num = int(input("Digite um número: "))
-    is_written = plc.write_by_name("GVL.tempo", num, pyads.PLCTYPE_INT)
+    is_written = plc.write_by_name("GVL.variable", num, pyads.PLCTYPE_INT)
     
-    result = plc.read_by_name("GVL.tempo", pyads.PLCTYPE_INT)
+    result = plc.read_by_name("GVL.variable", pyads.PLCTYPE_INT)
     print(result)
